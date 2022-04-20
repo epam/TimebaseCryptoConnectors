b3b5d675e5e4919a0b0cbaaf0e6f54909ee5d0ed
gudkov <gudkovanatoly@hotmail.com>
2022-04-20 14:05:04 +0300

    Integration testing - 1st prototype of an int test

--- java/commons/src/testFixtures/java/com/epam/deltix/data/connectors/test/fixtures/integration/TbIntTestPreparation.java
+++ java/commons/src/testFixtures/java/com/epam/deltix/data/connectors/test/fixtures/integration/TbIntTestPreparation.java
@@ -1,31 +1,18 @@
 package com.epam.deltix.data.connectors.test.fixtures.integration;
 
-import org.junit.Rule;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.utility.DockerImageName;
 
 public class TbIntTestPreparation {
-    public static Network network = Network.newNetwork();
-
     public static final String TIMEBASE_SERVER_VERSION = System.getProperty("timebaseServerVersion", "6.1");
+
+    public static final Network NETWORK = Network.newNetwork();
 
     public static GenericContainer TIMEBASE_CONTAINER = new GenericContainer(
             DockerImageName.parse("finos/timebase-ce-server:" + TIMEBASE_SERVER_VERSION))
-            .withNetwork(network)
-            .withNetworkAliases("timebase")
             .withReuse(true)
+            .withNetwork(NETWORK)
+            .withNetworkAliases("timebase")
             .withExposedPorts(8011);
-
-    @BeforeAll
-    public static void beforeAll() {
-        TIMEBASE_CONTAINER.start();
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        TIMEBASE_CONTAINER.stop();
-    }
 }
--- java/runner/src/intTest/java/com/epam/deltix/data/connectors/ApplicationIntTest.java
+++ java/runner/src/intTest/java/com/epam/deltix/data/connectors/ApplicationIntTest.java
@@ -1,19 +1,26 @@
 package com.epam.deltix.data.connectors;
 
 import com.epam.deltix.data.connectors.test.fixtures.integration.TbIntTestPreparation;
+import com.epam.deltix.qsrv.hf.tickdb.pub.DXTickDB;
+import com.epam.deltix.qsrv.hf.tickdb.pub.DXTickStream;
+import com.epam.deltix.qsrv.hf.tickdb.pub.SelectionOptions;
+import com.epam.deltix.qsrv.hf.tickdb.pub.TickCursor;
+import com.epam.deltix.qsrv.hf.tickdb.pub.TickDBFactory;
+import com.epam.deltix.qsrv.hf.tickdb.pub.TimeConstants;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import java.nio.file.Path;
 import java.time.Duration;
-import java.util.function.Consumer;
-
-import static org.testcontainers.containers.output.OutputFrame.OutputType.STDERR;
-import static org.testcontainers.containers.output.OutputFrame.OutputType.STDOUT;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class ApplicationIntTest extends TbIntTestPreparation {
 
@@ -21,41 +28,75 @@ public class ApplicationIntTest extends TbIntTestPreparation {
             new ImageFromDockerfile("localhost/timebase-crypto-connectors:snapshot")
                     .withFileFromPath("Dockerfile", Path.of("src/main/docker/Dockerfile"))
                     .withFileFromPath(".", Path.of("build/docker"))).
-            withNetwork(network).
             withReuse(true).
-            withEnv("JAVA_OPTS", "-Dtimebase.url=dxtick://timebase:8011").
-            withStartupTimeout(Duration.ofSeconds(240));
+            withNetwork(NETWORK).
+            //withExposedPorts(8055) // uncomment when the service really exposes the port when all DCs have been started
+                    withEnv("JAVA_OPTS", "-Dtimebase.url=dxtick://timebase:8011").
+                    withStartupTimeout(Duration.ofSeconds(240));
 
     @BeforeAll
-    public static void beforeAll() {
-        TbIntTestPreparation.beforeAll();
-
+    static void beforeAll() throws Exception {
+        TIMEBASE_CONTAINER.start();
         APP_CONTAINER.start();
 
-        APP_CONTAINER.followOutput(new Consumer<OutputFrame>() {
-            @Override
-            public void accept(final OutputFrame outputFrame) {
-                System.out.print("O>>>" + outputFrame.getUtf8String());
-            }
-        }, STDOUT);
-        APP_CONTAINER.followOutput(new Consumer<OutputFrame>() {
-            @Override
-            public void accept(final OutputFrame outputFrame) {
-                System.out.print("E>>>" + outputFrame.getUtf8String());
-            }
-        }, STDERR);
+        Thread.sleep(5000); // let all DCs to be started. TODO: replace by port/8055 availability
+        // checking with specifying exposedPorts in the container specification
     }
 
     @AfterAll
-    public static void afterAll() {
+    static void afterAll() {
         APP_CONTAINER.stop();
-        TbIntTestPreparation.afterAll();
+        TIMEBASE_CONTAINER.stop();
     }
 
-    @Test
-    void dataFeed() throws Exception {
-        System.out.println("test1");
-        // TODO: implement test
-        //Thread.sleep(600000);
+    DXTickDB db;
+
+    @BeforeEach
+    void setupTb() {
+        db = TickDBFactory.createFromUrl("dxtick://localhost:" + TIMEBASE_CONTAINER.getFirstMappedPort());
+        db.open(true);
+    }
+
+    @AfterEach
+    void closeTb() {
+        db.close();
+    }
+
+    @TestFactory
+    Stream<DynamicTest> testDataFeedInTb() throws Exception {
+        final String[] connectors = new String[]{
+                "coinbase",
+                "kraken-spot"
+                /* TODO: read from application.yaml */
+        };
+
+        return Arrays.stream(connectors).map(connector -> DynamicTest.dynamicTest(
+                connector,
+                () -> tryReadPackageHeaders(connector)
+        ));
+    }
+
+    void tryReadPackageHeaders(final String connector) throws Exception {
+        final int expectedNumOfPackageHeader = 10;
+        final int timeoutSeconds = 5;
+
+        final DXTickStream stream = db.getStream(connector);
+
+        Assertions.assertNotNull(stream, "Connector " + connector + " not started as expected. " +
+                "No output stream found.");
+
+        try (TickCursor cursor = stream.select(TimeConstants.USE_CURRENT_TIME,
+                new SelectionOptions(true, true))) {
+
+            Assertions.assertTimeout(Duration.ofSeconds(timeoutSeconds), () -> {
+                int packageHeaders = 0;
+
+                while (cursor.next()) {
+                    if (++packageHeaders == expectedNumOfPackageHeader) {
+                        break;
+                    }
+                }
+            });
+        }
     }
 }
