7d9bcc5dcc006a168feeb41e56a7493c23b40748
Romkisel <romkisel@gmail.com>
2022-04-22 14:36:08 +0300

    bitmart support

---
+++ java/connectors/bitmart/src/main/java/com/epam/deltix/data/connectors/bitmart/BitmartSpotDataConnector.java
@@ -0,0 +1,35 @@
+package com.epam.deltix.data.connectors.bitmart;
+
+import com.epam.deltix.data.connectors.commons.*;
+import com.epam.deltix.data.connectors.commons.annotations.Connector;
+
+@Connector("BITMART")
+public class BitmartSpotDataConnector extends DataConnector<BitmartSpotConnectorSettings> {
+    public BitmartSpotDataConnector(BitmartSpotConnectorSettings settings) {
+        super(settings, MdModel.availability()
+            .withTrades()
+            .withLevel1()
+            .withLevel2().build()
+        );
+    }
+
+    @Override
+    protected RetriableFactory<MdFeed> doSubscribe(
+            final MdModel.Options selected,
+            final CloseableMessageOutputFactory outputFactory,
+            final String... symbols) {
+
+        return errorListener -> {
+            final BitmartSpotFeed result = new BitmartSpotFeed(
+                settings().getWsUrl(),
+                settings().getDepth(),
+                selected,
+                outputFactory.create(),
+                errorListener,
+                symbols);
+            result.start();
+            return result;
+        };
+    }
+
+}
