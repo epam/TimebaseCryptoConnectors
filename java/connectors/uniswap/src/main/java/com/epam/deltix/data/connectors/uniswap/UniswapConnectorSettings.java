a428732dd4beab6fa260d88905d9d993447d84c2
gudkov <gudkovanatoly@hotmail.com>
2022-05-25 13:23:17 +0300

    Uniswap - paginating requires ordering

---
+++ java/connectors/uniswap/src/main/java/com/epam/deltix/data/connectors/uniswap/UniswapConnectorSettings.java
@@ -0,0 +1,29 @@
+package com.epam.deltix.data.connectors.uniswap;
+
+import com.epam.deltix.data.connectors.commons.DataConnectorSettings;
+import com.epam.deltix.data.connectors.commons.annotations.ConnectorSettings;
+
+@ConnectorSettings("UNISWAP")
+public class UniswapConnectorSettings extends DataConnectorSettings {
+
+    private String subgraphUrl = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-subgraph";
+
+    public UniswapConnectorSettings() {
+        super();
+    }
+
+    public UniswapConnectorSettings(final String name, final String tbUrl, final String stream) {
+        super(tbUrl, stream);
+        setName(name);
+        setSubgraphUrl(subgraphUrl);
+    }
+
+    public String getSubgraphUrl() {
+        return subgraphUrl;
+    }
+
+    public void setSubgraphUrl(String subgraphUrl) {
+        this.subgraphUrl = subgraphUrl;
+    }
+
+}
---
+++ java/connectors/uniswap/src/main/java/com/epam/deltix/data/connectors/uniswap/UniswapDataConnector.java
@@ -0,0 +1,69 @@
+package com.epam.deltix.data.connectors.uniswap;
+
+import com.epam.deltix.data.connectors.commons.CloseableMessageOutputFactory;
+import com.epam.deltix.data.connectors.commons.DataConnector;
+import com.epam.deltix.data.connectors.commons.MdFeed;
+import com.epam.deltix.data.connectors.commons.MdModel;
+import com.epam.deltix.data.connectors.commons.RetriableFactory;
+import com.epam.deltix.data.connectors.commons.annotations.Connector;
+import com.epam.deltix.data.uniswap.PoolAction;
+import com.epam.deltix.data.uniswap.TokenAction;
+
+@Connector("UNISWAP")
+public class UniswapDataConnector extends DataConnector<UniswapConnectorSettings> {
+    public UniswapDataConnector(final UniswapConnectorSettings settings) {
+        super(settings, MdModel.availability()
+            .withCustom(
+                    PoolAction.class,
+                    TokenAction.class)
+                .build()
+        );
+    }
+
+    @Override
+    protected RetriableFactory<MdFeed> doSubscribe(
+            final MdModel.Options selected,
+            final CloseableMessageOutputFactory outputFactory,
+            final String... symbols) {
+        return errorListener -> {
+
+            final UniswapFeed result = new UniswapFeed(
+                    settings().getSubgraphUrl(),
+                    selected,
+                    outputFactory.create(),
+                    errorListener,
+                    logger(),
+                    5_000
+                    );
+            result.start();
+            return result;
+        };
+    }
+
+    public static void main(String[] args) throws Exception {
+        final UniswapDataConnector dataConnector = new UniswapDataConnector(
+                new UniswapConnectorSettings(
+                        "uniswap",
+                        "dxtick://localhost:8011",
+                        "uniswap"
+                )
+        );
+
+        final MdModel model = dataConnector.model();
+
+        final MdModel.Availability availability = model.available();
+        dataConnector.logger().info(() -> availability.toString());
+
+        dataConnector.subscribe(
+                model.select().
+                        withCustom(PoolAction.class/*, TokenAction.class*/).
+                        build()
+        );
+
+        System.in.read();
+
+        dataConnector.close();
+
+        dataConnector.logger().info(() -> "CLOSED");
+    }
+}
